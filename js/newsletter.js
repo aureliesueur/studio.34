7adb04f67b37771f672f2c416cfce8edd6825fb3
Aurélie Sueur <sueuraurelie@sfr.fr>
2021-05-05 10:57:53 +0200

    Version définitive du site vitrine de studio.34

---
+++ js/newsletter.js
@@ -0,0 +1,7 @@
+let btnClose = document.getElementById("close-popup");
+let newsPopup = document.getElementById("news-popup");
+
+btnClose.addEventListener("click", () => {
+   newsPopup.style.display = "none";
+});
+  
